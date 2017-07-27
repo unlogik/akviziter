3a22e0954b76f74df47ac3a98a1f3b0795a45368
unlogik <unlogik@gmail.com>
2017-07-27 15:36:16 +0200

    ---

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function(Controller) {
 	"use strict";
 
-	return Controller.extend("opensap.manageproducts.controller.BaseController", {
+	return Controller.extend("bp.controller.BaseController", {
 		/**
 		 * Convenience method for accessing the router.
 		 * @public
